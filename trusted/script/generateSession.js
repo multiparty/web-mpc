b76a1ed38af9e8ecc233bfb7bf11d8b4a1ea9d46
Eric Dunton <edunton@bu.edu>
2015-04-05 19:50:59 -0400

    modified:   trusted/script/generateSession.js

--- trusted/script/generateSession.js
+++ trusted/script/generateSession.js
@@ -42,7 +42,7 @@ function generateTable(tableBody,sessionID,status,timestamp,counter){
     $.ajax({
         type: "POST",
         url:"/get_data",
-        data: {session: sessionID, lastFetch: timestamp},
+        data: {session: sessionID, last_fetch: timestamp},
         dataType: "json",
         error: function(){
             console.log("error connecting");
