f85ab00770c9df45fe53558fbe4568c807e155c1
Kyle Holzinger <kylelholzinger@gmail.com>
2015-04-05 21:28:58 -0400

    Initial finialized product. Everything working.

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ try {
 }
 
 // model for aggregate data
-var Aggregate = mongoose.model('Aggregate', { fields: Object, date: Number, session: Number, email: String });
+var Aggregate = mongoose.model('Aggregate', { _id: String, fields: Object, date: Number, session: Number, email: String });
 var Mask = mongoose.model('Mask', { fields: Object, session: Number })
 var PublicKey = mongoose.model('PublicKey', { _id: Number, session: Number,
     pub_key: String});
@@ -102,7 +102,6 @@ app.post('/', function (req, res) {
     // verify that the json follows the schema as the data in the 
     // database and is actually json.
 
-    console.dir(req.body);
     if (  !(req.body.hasOwnProperty("mask") &&
         req.body.hasOwnProperty("data") &&
         req.body.hasOwnProperty("session") &&
@@ -121,17 +120,26 @@ app.post('/', function (req, res) {
         }
     }
 
-    var agg_to_save = new Aggregate({ fields: to_add, date: Date.now(),
+    var agg_to_save = new Aggregate({ _id: req.body.user, fields: to_add, date: Date.now(),
         session: req.body.session, email: req.body.user });
 
     var mask_to_save = new Mask({ fields: req.body.mask,
         session: req.body.session });
 
+    Aggregate.update({_id: req.body.user}, agg_to_save.toObject(), {upsert: true}, function (err) {
+        if (err) {
+            console.log(err);
+            res.status(503).json({error: "Unable to save aggregate, please try again"});
+        } else { }
+    })
+
+    /*
     agg_to_save.save(function (err) {
         if (err) {
             res.status(503).json({error: "Unable to save aggregate, please try again"});
         }
     });
+*/
 
     mask_to_save.save(function (err) {
         if (err) {
@@ -143,14 +151,12 @@ app.post('/', function (req, res) {
 });
 
 app.post("/publickey", function (req,res) {
-    console.log(req.body);
     PublicKey.findOne( {session: req.body.session }, function (err, data) {
         if (err) {
             console.log(err);
             res.send(err);
         }
 
-        console.log(data);
         if (data == null ) {
             res.json({error: "No key found with the specified session ID"});
         } else {
@@ -161,8 +167,9 @@ app.post("/publickey", function (req,res) {
 });
 
 app.post('/create_session', function (req, res) {
+    console.log('Generating session...');
     try {
-        var new_key = new PublicKey({session: req.body.session, pub_key: req.body.pub_key, _id: req.body.session});
+        var new_key = new PublicKey({session: req.body.session, pub_key: req.body.publickey, _id: req.body.session});
     } catch (err) {
         res.send(err);
     }
@@ -201,15 +208,16 @@ app.post('/get_masks', function (req, res) {
 
 app.post('/submit_agg', function (req, res) {
     var mask = req.body.data;
-    console.log(req.body);
+    //console.log(req.body);
+    console.log("Fetching aggregate");
     Aggregate.where({session: req.body.session}).find(function (err, data) {
         var final_data = {};
         for (row in data) {
-            for (field in row) {
+            for (field in data[row].fields) {
                 if (final_data[field] == undefined ){
                     final_data[field] = 0;
                 } 
-                final_data[field] += row[field];
+                final_data[field] += data[row].fields[field];
             }
         }
 
@@ -219,17 +227,22 @@ app.post('/submit_agg', function (req, res) {
             }
         }
 
+        console.log('Final aggregate conputed');
+
         var new_final = {_id: req.body.session, aggregate: final_data, date: Date.now(), session: req.body.session};
         var to_save = new FinalAggregate(new_final);
 
-        to_save.save(function (err){
+        
+        console.log('Saving aggregate');
+
+        FinalAggregate.update({_id: req.body.session}, to_save.toObject(), function (err) {
             if (err) {
                 console.log(err);
-                res.send(err);
             } else {
-                res.json(final_data);
+                console.log('Aggregate saved and sent');
             }
-        })
+        });
+        res.json(final_data);
 
     });
 })
--- unmask/script/unmask.js
+++ unmask/script/unmask.js
@@ -1,11 +1,20 @@
 //takes callback(true|false, data)
-function unmask(maskedData,decryptObj,session,callback){
+function unmask(mOut,decryptObj,session,callback){
+    mOut = JSON.parse(mOut.data);
+    var maskedData = [];
+
+    //console.log(mOut);
+    for (row in mOut) {
+        maskedData.push(mOut[row].fields);
+    }
+
+    console.log(maskedData);
     var decryptedJson = _.map(maskedData,function(submittion){
         return _.mapObject(submittion, function(val,key){
             return decryptObj.decrypt(val);
         });
     });
-    
+
     var keys = _.keys(decryptedJson[0]),
         aggObj = _.object(keys, _.map(_.range(keys.length), 
     											function (x) { return 0;}));
@@ -25,9 +34,13 @@ function unmask(maskedData,decryptObj,session,callback){
     $.ajax({
         type: "POST",
         url: "/submit_agg",
-        data: {data: aggObj, session: session},
+        contentType: "application/json",
+        data: JSON.stringify({data: aggObj, session: session}),
         dataType: "json",
-        success: function(data){callback(true,data)},
+        success: function(data){
+            console.log(data);
+            callback(true,data);
+        },
         error: function(){callback(false,"submittion to server failed")}
     });
 }
