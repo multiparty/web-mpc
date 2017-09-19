798622c754fc64c44f5f3c139f69f6e910e5eb23
lucyq <lucyzqin@gmail.com>
2017-09-19 09:50:18 -0400

    drag and drop

--- client/app/views/unmaskView.js
+++ client/app/views/unmaskView.js
@@ -1,128 +1,115 @@
-define(['jquery','controllers/unmaskController', 'helper/sail_hot', 'Ladda'], function ($, unmaskController, sailHOT, Ladda) {
+define(['jquery','controllers/unmaskController', 'helper/sail_hot', 'helper/drop_sheet', 'spin', 'Ladda', 'alertify'], 
+function ($, unmaskController, sailHOT, DropSheet, Spinner, Ladda, alertify) {
+
+  function callb() {
+    $("#tables-area").show();
+  }
+
+  function handleFile(f) {
+    if (!f.name) {
+      f = f.target.files[0]
+    }
     
+    if (f) {
+      var keyReader = new FileReader()
+      keyReader.readAsText(f);
+
+      $(keyReader).on('load', function (event){
+
+        var sessionKey = $('#session').val();
+        var sessionPassword = $('#session-password').val();
+        var privateKey = event.target.result;
+   
+        privateKey = privateKey.split('\n')[1];
+
+        $.ajax({
+          type: 'POST',
+          url: '/get_masks',
+          contentType: 'application/json',
+          data: JSON.stringify({session: sessionKey, password: sessionPassword}),
+          success: function(data) {
+            unmaskController.aggregate_and_unmask(data, privateKey, sessionKey, sessionPassword, callb)
+          },
+          error: function() {
+            // TODO:
+            console.log("FAILED")
+          }
+        });
+      });
+    }
+  }
+
+  function handleDragover(e) {
+
+    if (typeof jQuery !== 'undefined') {
+      e.stopPropagation();
+      e.preventDefault();
+      e.dataTransfer.dropEffect = 'copy';
+      $('#drop-area').removeClass('dragdefault');
+      $('#drop-area').addClass('dragenter');
+    } else {
+      alertify.alert("<img src='/images/cancel.png' alt='Error'>Error!", "Drag and drop not supported. Please use the 'Choose File' button or copy-and-paste data.");
+    }
+  }
+
+  function handleDragLeave(e) {
+      if (typeof jQuery !== 'undefined') {
+        $('#drop-area').removeClass('dragenter');
+      } else {
+        alertify.alert("<img src='/images/cancel.png' alt='Error'>Error!", "Drag and drop not supported. Please use the 'Choose File' button or copy-and-paste data.");
+      }
+  }
+
+
+  function dropSheetListen() {
+    
+    $('#unmask').removeAttr('disabled');
+    var target = document.getElementById('drop-area');
+    if (target) {
+
+      target.addEventListener('click', function(){
+        $('#choose-file').click();
+        $('#choose-file').change(handleFile);
+      
+      });
+
+      target.addEventListener('drop', function(e) {
+        if (typeof jQuery) {
+          e.stopPropagation();
+          e.preventDefault();
+        }
+
+        // check if pending?
+        var files = e.dataTransfer.files;
+        handleFile(files[0])
+      });
+
+      target.addEventListener('dragenter', handleDragover, false);
+      target.addEventListener('dragleave', handleDragLeave);
+      target.addEventListener('dragover', handleDragover, false);
+
+    }
+  }
+
+
+
   function unmaskView() {
       
     $(document).ready(function () {
-      //$('#friendly').hide();
+      $('#tables-area').hide();
 
       var req = $.ajax({
         type: 'GET',
         url: '/templates/tables.json',
         dataType: 'json'
       }).then(function (tables_def) {
-        var tables = sailHOT.make_tables(tables_def);
-        window.scrollTo(0, 0);
-        var tables_map = {};
-        for (var v = 0; v < tables.length; v++) {
-          tables_map[tables[v]._sail_meta.name] = tables[v];
-        }
-
-        //$('#friendly').hide();
-        $('#unmask').removeAttr('disabled');
-
-        // Handler for unmasking
-        $('#unmask').on('click', function () {
-          var maskKey = $('#maskKey').get(0),
-            sessKey = $('#sessKey').val(),
-            password = $('#password').val(),
-            la = Ladda.create(this),
-            keyReader = new FileReader();
-
-          if (maskKey.files.length) {
-            la.start();
-            keyReader.readAsText(maskKey.files[0]);
-            $(keyReader).on('load', function (e) {
-              var privateKey = e.target.result;
-
-              // Callback: display results in HOT
-              var callb = function (success, data) {
-                la.stop();
-
-                if (success) { // Display result
-                  for (var name in data) {
-                    if (!(data.hasOwnProperty(name))) {
-                      continue;
-                    }
-
-                    var table = tables_map[name];
-                    if (table) { // This is a table field
-
-                      var table_data = data[name];
-                      sailHOT.remove_validators(table);
-                      sailHOT.fill_data(table_data, table);
-                      sailHOT.read_only_table(table, true);
-
-
-                      // Show tables
-                      $('#' + table._sail_meta.element).show();
-                      $('#' + table._sail_meta.element + '-name').show();
-                 
-                    } else { // Not a table, questions
-                      var fields = data[name];
-
-                      var h3 = $('<h3>').text(toTitleCase(name));
-                      $('#' + name).append(h3);
-                      h3.show();
-
-                      for (var field in fields) {
-                        if (!(fields.hasOwnProperty(field))) {
-                          continue;
-                        }
-
-
-                        var ul = $('<ul>');
-                        var answers = fields[field];
-                        for (var option in answers) {
-                          if (!(answers.hasOwnProperty(option))) {
-                            continue;
-                          }
-
-                          var text = option + ': ' + answers[option];
-                          ul.append($('<li>').text(text));
-                        }
-
-                        $('#' + name).append($('<h4>').text(field));
-                        $('#' + name).append(ul);
-                        $('#' + name).append($('<br>'));
-                        $('#' + name).show();
-                      }
-                    }
-                  }
-
-                  // Display raw data.
-                  //$('#result').text(JSON.stringify(data, null, 4));
-                  //$('#result').show();
-                  $('#error').hide();
-                  $('#unmask').prop('disabled', true);
-                  $('#unmask').hide();
-                }
-
-                else { // !success, display error
-                  $('#error').text(data);
-                  $('#error').show();
-                }
-              };
-
-              // Remove top and bottom line of pem file
-              privateKey = privateKey.split('\n')[1];
-    
-              $.ajax({
-                type: 'POST',
-                url: '/get_masks',
-                contentType: 'application/json',
-                data: JSON.stringify({session: sessKey, password: password}),
-                success: function (data) {
-                  unmaskController.aggregate_and_unmask(data, privateKey, sessKey, password, callb);
-                },
-                error: function () {
-                  callb(false, 'Error: failed to load masks');
-                }
-              });
-            });
-          } else {
-            alert('Please upload both files before continuing.');
-          }
-        });
+        // var tables = sailHOT.make_tables(tables_def);
+        // window.scrollTo(0, 0);
+        // var tables_map = {};
+        // for (var v = 0; v < tables.length; v++) {
+        //   tables_map[tables[v]._sail_meta.name] = tables[v];
+        // }
+        dropSheetListen();
       });
     });
 
