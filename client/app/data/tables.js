e7032a8285e421da2c438d8c8d24b90b463807b1
Frederick Jansen <frederick.jansen@gmail.com>
2018-04-20 12:05:59 -0400

    Split data into multiple tables

--- client/app/data/tables.js
+++ client/app/data/tables.js
@@ -2,27 +2,156 @@ define([], function () {
   return {
     tables: [
       {
-        "name": "Pacesetter Procurement Measure",
+        "name": "Amount spent with MBEs",
         "element": "pacesetter-measure-hot",
         "hot_parameters": {
           "rowHeaderWidth": 480,
-          "height": 275,
+          "height": 230,
           "colWidths": [190],
           "stretchH": "last"
         },
         "rows": [
           {
             "key": "DollarAmtLocal",
-            "label": "Dollar Amount Spent with Local MBEs in Whole Dollars"
-          },{
-            "key": "DollarAmt",
-            "label": "Dollar Amount Spent with MBEs in Whole Dollars"
-          },{
-            "key": "TotalAmt",
-            "label": "Total Dollar Amount Spent Procuring All Goods and Services in the United States in Whole Dollars"
-          },{
-            "key": "NumContracted",
-            "label": "Number of Local MBEs With Whom You Have Done Business"
+            "label": "Dollar Amount Spent with <i>Local</i> MBEs"
+          }, {
+            "key": "DollarAmtState",
+            "label": "Dollar Amount Spent with <i>State</i> MBEs"
+          }, {
+            "key": "DollarAmtNational",
+            "label": "Dollar Amount Spent with <i>National</i> MBEs"
+          }
+        ],
+        "cols": [
+          [
+            {
+              "label": "Value for FY17 in Thousands of Dollars",
+              "key": "value"
+            }
+          ]
+        ],
+        "types": [
+          {
+            "range": {
+              "row": "0:2",
+              "col": "*"
+            },
+            "type": "currency",
+            "min": 0,
+            "max_warning": 1000000,
+            "empty": false,
+            "validators": [
+              "discrepancies"
+            ]
+          }
+        ],
+        "excel": [
+          {
+            "sheet": "Pacesetters",
+            "start": "B7",
+            "end": "Q16",
+            "firstrow": "Dollar Amount Spent with Local MBEs"
+          }
+        ],
+        "tooltips": [
+          {
+            "range": {
+              "row": "*",
+              "col": "*"
+            },
+            "tooltip": {
+              "errorTitle": "Invalid Data Entry",
+              "error": "Please do not input any text or leave any cells blank. If the value is zero, please input zero.",
+              "warningTitle": "Warning: Data is too big",
+              "warning": "Are you sure this value is correct?"
+            }
+          }
+        ]
+      }, {
+        "name": "Addressable spend",
+        "element": "pacesetter-measure-hot-2",
+        "hot_parameters": {
+          "rowHeaderWidth": 480,
+          "height": 230,
+          "colWidths": [190],
+          "stretchH": "last"
+        },
+        "rows": [
+          {
+            "key": "TotalAmtLocal",
+            "label": "Total Dollar Amount Spent Procuring All Goods and Services <i>Locally</i>"
+          }, {
+            "key": "TotalAmtState",
+            "label": "Total Dollar Amount Spent Procuring All Goods and Services at the <i>State Level</i>"
+          }, {
+            "key": "TotalAmtNational",
+            "label": "Total Dollar Amount Spent Procuring All Goods and Services in the <i>United States</i>"
+          }
+        ],
+        "cols": [
+          [
+            {
+              "label": "Value for FY17 in Thousands of Dollars",
+              "key": "value"
+            }
+          ]
+        ],
+        "types": [
+          {
+            "range": {
+              "row": "0:2",
+              "col": "*"
+            },
+            "type": "currency",
+            "min": 0,
+            "max_warning": 1000000,
+            "empty": false,
+            "validators": [
+              "discrepancies"
+            ]
+          }
+        ],
+        "excel": [
+          {
+            "sheet": "Pacesetters",
+            "start": "B7",
+            "end": "Q16",
+            "firstrow": "Dollar Amount Spent with Local MBEs"
+          }
+        ],
+        "tooltips": [
+          {
+            "range": {
+              "row": "*",
+              "col": "*"
+            },
+            "tooltip": {
+              "errorTitle": "Invalid Data Entry",
+              "error": "Please do not input any text or leave any cells blank. If the value is zero, please input zero.",
+              "warningTitle": "Warning: Data is too big",
+              "warning": "Are you sure this value is correct?"
+            }
+          }
+        ]
+      }, {
+        "name": "Number of MBEs",
+        "element": "pacesetter-measure-hot-3",
+        "hot_parameters": {
+          "rowHeaderWidth": 480,
+          "height": 230,
+          "colWidths": [190],
+          "stretchH": "last"
+        },
+        "rows": [
+          {
+            "key": "NumContractedLocal",
+            "label": "Number of <i>Local</i> MBEs With Whom You Have Done Business"
+          }, {
+            "key": "NumContractedState",
+            "label": "Number of <i>State</i> MBEs With Whom You Have Done Business"
+          }, {
+            "key": "NumContractedNational",
+            "label": "Number of <i>National</i> MBEs With Whom You Have Done Business"
           }
         ],
         "cols": [
@@ -39,22 +168,9 @@ define([], function () {
               "row": "0:2",
               "col": "*"
             },
-            "type": "currency",
-            "min": 0,
-            "max_warning": 1000000000,
-            "empty": false,
-            "validators": [
-              "discrepancies"
-            ]
-          },
-          {
-            "range": {
-              "row": "3",
-              "col": "*"
-            },
             "type": "int",
             "min": 0,
-            "max_warning": 1000,
+            "max_warning": 10000,
             "empty": false,
             "validators": [
               "discrepancies"
