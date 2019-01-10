8be661316bce6718b3bfeb502086567a1a5ff8af
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2019-01-09 22:30:27 -0500

    Implement mailboxes based on database

--- server/jiff/create.js
+++ server/jiff/create.js
@@ -2,11 +2,10 @@ const jiffServer = require('../../jiff/lib/jiff-server.js');
 const jiffServerBigNumber = require('../../jiff/lib/ext/jiff-server-bignumber.js');
 const jiffServerRestAPI = require('../../jiff/lib/ext/jiff-server-restful.js');
 
-const options = { logs: false, sodium: false, hooks: {} };
+const options = { logs: true, sodium: false, hooks: {} };
 
 module.exports = function (server) {
-  // var mailbox_hooks = require('./mailbox.js');
-  var mailbox_hooks = {};
+  var mailbox_hooks = require('./mailbox.js');
   var authentication_hooks = require('./auth.js');
 
   options.hooks = Object.assign(options.hooks, mailbox_hooks, authentication_hooks);
--- server/jiff/mailbox.js
+++ server/jiff/mailbox.js
@@ -1,13 +1,64 @@
+const modules = require('../modules/modules.js');
+var Promise = require('bluebird');
+
 module.exports = {
-  put_in_mailbox: function (label, msg, computation_id, to_id) {
+  put_in_mailbox: function (jiff, label, msg, computation_id, to_id) {
     // to_id: either 1 or s1
     // computation_id: same as session key
     // msg JSON string
     // label string: share / open / etc ..
+    return new Promise(function (resolve, reject) {
+      var tmp = JSON.parse(msg);
+      if (label === 'public_keys') {
+        tmp['party_id'] = 's1';
+        tmp['op_id'] = 'public_keys';
+      }
 
+      var id = computation_id + ':' + tmp['party_id'] + ':' + to_id + ':' + tmp['op_id'];
+      var obj = new modules.Mailbox({
+        _id: id,
+        session: computation_id,
+        from_id: tmp['party_id'],
+        to_id: to_id,
+        op_id: tmp['op_id'],
+        label: label,
+        message: msg
+      });
+
+      var promise = modules.Mailbox.update(
+        { _id: id },
+        obj.toObject(),
+        { upsert: true }
+      );
+
+      promise.then(resolve).catch(function () {
+        reject('Unable to save aggregate, please try again.');
+      });
+    });
   },
-  get_mailbox : function (session_key, party_id) {
+
+  get_mailbox : function (jiff, computation_id, to_id) {
     // party_id: either 1 or s1
+    return new Promise(function (resolve, reject) {
+      modules.Mailbox.where({ to_id: to_id, session: computation_id }).find(function (err, data) {
+        if (err) {
+          console.log(err);
+          reject('Error getting masks.');
+          return;
+        }
+
+        if (!data || data.length === 0) {
+          resolve([]);
+          // TODO: move this to the mpc computation code: reject('No submissions yet. Please come back later.');
+        } else {
+          var result = [];
+          for (var d of data) {
+            result.push({ msg: d.message, label: d.label });
+          }
+          resolve(result);
+        }
+      });
+    });
   },
 
   // Do not remove anything from the mailbox/db ever
