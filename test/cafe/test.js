f5e6262634ed57f71ed82a4e3d24ea80f197f8d1
lucyq <lucyzqin@gmail.com>
2019-02-01 22:57:40 -0500

    basic test infra

--- test/cafe/test.js
+++ test/cafe/test.js
@@ -7,15 +7,32 @@ fixture `Creating a session`
         .typeText('#session-title', 'testing!')
         .typeText('#session-description', 'a test description')
         .click('#generate')
-
-
   });
 
 fixture `Starting a session`
   .page `localhost:8080/manage`;
   test('Starting a session', async t => {
     await t
-    
-
+      .typeText('#session', '87j4egqf4jck65djg98z5r0rac')
+      .typeText('#password', 'w35ajn6g43bx9yzdy8jh6dy26c')
+      .click('#login')
+      // .click('#session-start')
   });
 
+
+const fileUpload = Selector('#choose-file');
+
+fixture `Submitting data`
+  .page `localhost:8080/`;
+  test('Participant 1', async t => {
+    await t
+      .click('#session')
+      .typeText('#session', '87j4egqf4jck65djg98z5r0rac')
+      .click('#participation-code')
+      .typeText('#participation-code', 'd7fbq0kxxbyxy8ghcbr505d8tg')
+      .click('#expand-table-button')
+      .setFilesToUpload(fileUpload, '/Users/lucyqin/Desktop/pace.xlsx')
+      .click('#verify')
+
+
+  });
