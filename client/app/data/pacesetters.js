4e150cb6bd9fe7d89eeececf01f45b01cc2d8aa6
Ira Globus-Harris <igh@bu.edu>
2019-02-21 14:46:07 -0500

    removed highlighting in the hackiest way possible, fixed height issue, added in units

--- client/app/data/pacesetters.js
+++ client/app/data/pacesetters.js
@@ -10,28 +10,28 @@ define([], function () {
         "element": "amount-spent",
         "hot_parameters": {
           "rowHeaderWidth": 500,
-          "height": 325,
+          // "height": 325,
           "colWidths": 380
         },
         "rows": [
           {
             "key": "local",
-            "label": "Dollar Amount Spent with Local MBEs"
+            "label": "Dollar Amount Spent with Local MBEs ($)"
           },
           {
             "key": "state",
-            "label": "Dollar Amount Spent with State MBEs"
+            "label": "Dollar Amount Spent with State MBEs ($)"
           },
           {
             "key": "national",
-            "label": "Dollar Amount Spent with National MBEs"
+            "label": "Dollar Amount Spent with National MBEs ($)"
           }
         ],
         "cols": [
           [
             {
               "key": "value",
-              "label": "Value for FY in Thousands of Dollars"
+              "label": "Value for FY in Thousands of Dollars ($)"
             }
           ]
         ],
@@ -52,22 +52,7 @@ define([], function () {
             "sheet": "1. Amount Spent",
             "start": "B2",
             "end": "B4",
-            "firstrow": "Dollar Amount Spent with Local MBEs"
-          }
-        ],
-        "tooltips": [
-          {
-            "range": {
-              "row": "*",
-              "col": "*"
-
-            },
-            "tooltip": {
-              "errorTitle": "Invalid Data Entry",
-              "error": "Please do not input any text or leave any cells blank. If the value is zero, please input zero.",
-              "warningTitle": "Warning: Data is too big",
-              "warning": "Are you sure this value is correct?"
-            }
+            "firstrow": "Dollar Amount Spent with Local MBEs ($)"
           }
         ]
       },
@@ -76,28 +61,28 @@ define([], function () {
         "element": "addressable-spend",
         "hot_parameters": {
           "rowHeaderWidth": 500,
-          "height": 325,
+          // "height": 325,
           "colWidths": 380
         },
         "rows": [
           {
             "key": "local",
-            "label": "Total Dollar Amount Spent Procuring All Goods and Services Locally"
+            "label": "Total Dollar Amount Spent Procuring All Goods and Services Locally ($)"
           },
           {
             "key": "state",
-            "label": "Total Dollar Amount Spent Procuring All Goods and Services at the State Level"
+            "label": "Total Dollar Amount Spent Procuring All Goods and Services at the State Level ($)"
           },
           {
             "key": "national",
-            "label": "Total Dollar Amount Spent Procuring All Goods and Services in the United States"
+            "label": "Total Dollar Amount Spent Procuring All Goods and Services in the United States ($)"
           }
         ],
         "cols": [
           [
             {
               "key": "value",
-              "label": "Value for FY in Thousands of Dollars"
+              "label": "Value for FY in Thousands of Dollars ($)"
             }
           ]
         ],
@@ -118,22 +103,7 @@ define([], function () {
             "sheet": "2. Addressable Spend",
             "start": "B2",
             "end": "B4",
-            "firstrow": "Total Dollar Amount Spent Procuring All Goods and Services Locally"
-          }
-        ],
-        "tooltips": [
-          {
-            "range": {
-              "row": "*",
-              "col": "*"
-
-            },
-            "tooltip": {
-              "errorTitle": "Invalid Data Entry",
-              "error": "Please do not input any text or leave any cells blank. If the value is zero, please input zero.",
-              "warningTitle": "Warning: Data is too big",
-              "warning": "Are you sure this value is correct?"
-            }
+            "firstrow": "Total Dollar Amount Spent Procuring All Goods and Services Locally ($)"
           }
         ]
       },
@@ -142,21 +112,21 @@ define([], function () {
         "element": "number-MBEs",
         "hot_parameters": {
           "rowHeaderWidth": 500,
-          "height": 325,
+          // "height": 325,
           "colWidths": 380
         },
         "rows": [
           {
             "key": "local",
-            "label": "Number of Local MBEs With Whom You Have Done Business"
+            "label": "Number of Local MBEs With Whom You Have Done Business (#)"
           },
           {
             "key": "state",
-            "label": "Number of State MBEs With Whom You Have Done Business"
+            "label": "Number of State MBEs With Whom You Have Done Business (#)"
           },
           {
             "key": "national",
-            "label": "Number of National MBEs With Whom You Have Done Business"
+            "label": "Number of National MBEs With Whom You Have Done Business (#)"
           }
         ],
         "cols": [
@@ -177,29 +147,6 @@ define([], function () {
             "min": 0,
             "max_warning": 100,
             "empty": false,
-          }
-        ],
-        "excel": [
-          {
-            "sheet": "3. Number of MBEs",
-            "start": "B2",
-            "end": "B4",
-            "firstrow": "Number of Local MBEs With Whom You Have Done Business"
-          }
-        ],
-        "tooltips": [
-          {
-            "range": {
-              "row": "*",
-              "col": "*"
-
-            },
-            "tooltip": {
-              "errorTitle": "Invalid Data Entry",
-              "error": "Please do not input any text or leave any cells blank. If the value is zero, please input zero.",
-              "warningTitle": "Warning: Data is too big",
-              "warning": "Are you sure this value is correct?"
-            }
           }
         ]
       }
