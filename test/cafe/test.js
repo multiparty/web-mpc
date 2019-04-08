5fc33fdd45e5e7b9b04157da64c6276a5156e03d
lucyq <lucyzqin@gmail.com>
2019-04-08 13:43:32 -0400

    starting a session

--- test/cafe/test.js
+++ test/cafe/test.js
@@ -1,5 +1,9 @@
 import { Selector } from 'testcafe';
 
+
+let sessionKey = null;
+let sessionPassword = null;
+
 function createSession() {
   fixture `Creating a session`
   .page `localhost:8080/create`;
@@ -9,14 +13,31 @@ function createSession() {
         .typeText('#session-description', 'a test description')
         .click('#generate')
         .wait(2000);
+    
 
-    // sessionKey = (await Selector('#sessionID').innerText).trim();
-    // sessionPassword = (await Selector('#passwordID').innerText).trim();
-    // console.log(sessionKey, sessionPassword);
+    sessionKey = (await Selector('#sessionID').innerText).trim();
+    sessionPassword = (await Selector('#passwordID').innerText).trim();
+    console.log(sessionKey, sessionPassword);
   });
+}
+
+function startSession() { 
+  fixture `Start`
+    .page `localhost:8080/manage`;
+    test('Starting a session', async t => {
+      await t
+        .click('#session')
+        .typeText('#session', sessionKey)
+        .click('#password')
+        .typeText('#password', sessionPassword)
+        .click('#login')
+        .wait(2000)
+        .click('#session-start');
+    });
 }
 
 
 createSession();
+startSession();
 
 
