32b57f0aebcd8468dbfc5c40685818c21477282d
lucyq <lucyzqin@gmail.com>
2019-04-08 14:37:27 -0400

    config for travis

--- test/cafe/test.js
+++ test/cafe/test.js
@@ -37,6 +37,39 @@ function startSession() {
 }
 
 
+let participants = null;
+
+function getParticipationCodes() { 
+  fixture `Participation Codes`
+    .page `localhost:8080/manage`;
+    test('Generating Participation codes', async t => {
+      await t
+        .click('#session')
+        .typeText('#session', sessionKey)
+        .click('#password')
+        .typeText('#password', sessionPassword)
+        .click('#login')
+        .wait(3000)
+        .typeText('#participants-count', numberOfParticipants.toString())
+        .click('#participants-submit')
+        .wait(2000);
+
+      participants = await Selector('#participants-new').innerText;
+      participants = participants.trim().split('\n');
+      for (var i = 0; i < participants.length; i++) {
+        participants[i] = participants[i].trim();
+        if (participants[i] !== '') {
+          var index = participants[i].indexOf('participationCode') + 'participationCode'.length + 1;
+          participant_codes.push(participants[i].substring(index));
+        }
+      }
+
+      await t.expect(participant_codes.length).eql(numberOfParticipants);
+      console.log(participant_codes);
+    });
+}
+
+
 createSession();
 startSession();
 
