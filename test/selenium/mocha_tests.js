42335436bc630bf5a1eddf934c9d1b63c23f4a10
lucyq <lucyzqin@gmail.com>
2019-05-06 13:17:28 -0400

    testing travis

--- test/selenium/mocha_tests.js
+++ test/selenium/mocha_tests.js
@@ -1,9 +1,15 @@
 const assert = require('assert');
 // const test = require('selenium-webdriver/testing');
 const webdriver = require('selenium-webdriver');
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const {By, Key, until} = require('selenium-webdriver');
 const expect = require('chai').expect;
 let chrome = require('selenium-webdriver/chrome');
+let options = new chrome.Options();
+options.addArguments('headless'); // note: without dashes
+// options.addArguments('disable-gpu');
+options.addArguments('ignore-certificate-errors');
+
+
 let path = require('chromedriver').path;
 
 let sessionKey = null;
@@ -20,51 +26,55 @@ var service = new chrome.ServiceBuilder(path).build();
     chrome.setDefaultService(service);
 var driver = new webdriver.Builder()
     .forBrowser('chrome')
-    .withCapabilities(webdriver.Capabilities.chrome()) 
+    .withCapabilities(webdriver.Capabilities.chrome())
     .build();
 
 
-function getUserHome() {
-  return process.env.HOME || process.env.USERPROFILE;
-}
+// function getUserHome() {
+//   return process.env.HOME || process.env.USERPROFILE;
+// }
 
 
 describe('End-to-end workflow test', function() {
 
   it('Create session', async () => {
-
     await driver.get('localhost:8080/create')
       .then(() => driver.findElement(By.id('session-title')))
-      .then((title) =>  title.sendKeys('test-session'))
-      .then(() => driver.findElement(By.id('session-description')))
-      .then((description) => description.sendKeys('test-session description'))
-      .then(() => driver.findElement(By.id('generate')).click());
 
-    // Get session key
-    await driver.wait(function() {
-      return driver.findElement(By.id('sessionID')).isDisplayed();
-    }, 10000);
-    await driver.findElement(By.id('sessionID'))
-      .then(elem => elem.getText()
-        .then(function (text) {
-          sessionKey = text;
-          console.log('key', sessionKey);
-          expect(text.length).to.equal(26);
-        }));
+  });
 
-    // Get session password
-    await driver.wait(function() {
-      return driver.findElement(By.id('passwordID')).isDisplayed();
-    }, 10000);
-    //save sessionPassword
-    await driver.findElement(By.id('passwordID'))
-      .then(elem => elem.getText()
-        .then(function (text) {
-          sessionPassword = text;
-          console.log('sessionPs', sessionPassword);
-          expect(text.length).to.equal(26);
-        }));
-  });    
+//     await driver.get('localhost:8080/create')
+//       .then(() => driver.findElement(By.id('session-title')))
+//       .then((title) =>  title.sendKeys('test-session'))
+//       .then(() => driver.findElement(By.id('session-description')))
+//       .then((description) => description.sendKeys('test-session description'))
+//       .then(() => driver.findElement(By.id('generate')).click());
+
+//     // Get session key
+//     await driver.wait(function() {
+//       return driver.findElement(By.id('sessionID')).isDisplayed();
+//     }, 10000);
+//     await driver.findElement(By.id('sessionID'))
+//       .then(elem => elem.getText()
+//         .then(function (text) {
+//           sessionKey = text;
+//           console.log('key', sessionKey);
+//           expect(text.length).to.equal(26);
+//         }));
+
+//     // Get session password
+//     await driver.wait(function() {
+//       return driver.findElement(By.id('passwordID')).isDisplayed();
+//     }, 10000);
+//     //save sessionPassword
+//     await driver.findElement(By.id('passwordID'))
+//       .then(elem => elem.getText()
+//         .then(function (text) {
+//           sessionPassword = text;
+//           console.log('sessionPs', sessionPassword);
+//           expect(text.length).to.equal(26);
+//         }));
+//   });    
 
   // it ('Get participant links', async() => {
   //   await driver.wait(function() {
