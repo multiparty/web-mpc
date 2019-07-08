69ae8d9c0eaeaccf490791e81144fa586ea90d24
lucyq <lucyzqin@gmail.com>
2019-07-08 14:39:06 -0400

    styling and cleaning up

--- client/app/views/definitionsView.js
+++ client/app/views/definitionsView.js
@@ -1,26 +1,41 @@
-define(['jquery', 'table_template'], function ($, tableTemplate) {
+define(['jquery', 'Handsontable', 'table_template'], function ($, Handsontable, tableTemplate) {
 
   function definitionsView() {
-    $(document).ready(function () {
-      console.log(tableTemplate.definitions);
-      $("#definitions").append('<h2 class"text-center">' + tableTemplate.definitions.title + '</h2>')
+    function createTableHeader($tableDiv, headers) {
+      var $header = document.createElement('tr');
+      for (var i = 0; i < headers.length; i++) {
+        var $td = document.createElement('td');
+        $td.innerText = headers[i];
+        $td.setAttribute('class', 'font-weight-bold');
+        $header.append($td);
+      }
+      $tableDiv.append($header);
+    }
 
-      var $tableDiv = document.createElement('table');
-      $tableDiv.setAttribute('class', 'table table-striped')
-      
-      var table = tableTemplate.definitions.table;
-
+    function createTable($tableDiv, table) {
       for (var i = 0; i < table.length; i++) {
         var $tr = document.createElement('tr');
 
         for (var j = 0; j < table[i].length; j++) {
-
           var $td = document.createElement('td');
           $td.innerText = table[i][j];
+
+          if (j === 0) {
+            $td.setAttribute('class', 'font-weight-bold')
+          }
+
           $tr.append($td);
         }
         $tableDiv.append($tr);
       }
+    }
+
+    $(document).ready(function () {
+      $("#definitions").append('<h2 class="text-center">' + tableTemplate.definitions.title + '</h2>')
+
+      var $tableDiv = document.getElementById('definitions-table');
+      createTableHeader($tableDiv, tableTemplate.definitions.headers);
+      createTable($tableDiv, tableTemplate.definitions.table);
 
       document.getElementById('definitions').append($tableDiv);
     });
