0fab4be1d4a5c31bea03b104dd2d814e156be677
lucyq <lucyzqin@gmail.com>
2019-07-08 13:19:49 -0400

    rendering table

--- client/app/views/definitionsView.js
+++ client/app/views/definitionsView.js
@@ -1,8 +1,28 @@
-define(['jquery', 'table_template', 'bootstrap'], function ($, tableTemplate) {
+define(['jquery', 'table_template'], function ($, tableTemplate) {
 
   function definitionsView() {
     $(document).ready(function () {
-      console.log(tableTemplate);
+      console.log(tableTemplate.definitions);
+      $("#definitions").append('<h2 class"text-center">' + tableTemplate.definitions.title + '</h2>')
+
+      var $tableDiv = document.createElement('table');
+      $tableDiv.setAttribute('class', 'table table-striped')
+      
+      var table = tableTemplate.definitions.table;
+
+      for (var i = 0; i < table.length; i++) {
+        var $tr = document.createElement('tr');
+
+        for (var j = 0; j < table[i].length; j++) {
+
+          var $td = document.createElement('td');
+          $td.innerText = table[i][j];
+          $tr.append($td);
+        }
+        $tableDiv.append($tr);
+      }
+
+      document.getElementById('definitions').append($tableDiv);
     });
   }
 
