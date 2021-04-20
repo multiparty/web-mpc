df3dce3ae331efc4cac63a4135f805e1dc27c679
p-flock <pflock@bu.edu>
2021-04-19 22:13:47 -0400

    Add new fields for pacesetters 2021 data submission

--- client/app/data/pacesetters.js
+++ client/app/data/pacesetters.js
@@ -6,8 +6,8 @@ define([], function () {
   return {
     "tables": [
       {
-        "name": "Amount Spent with MBEs",
-        "element": "amount-spent",
+        "name": "Total Dollars Spent with Minority-Owned Business Enterprises ",
+        "element": "amount-spent", //TODO: what does this element do?
         "hot_parameters": {
           "rowHeaderWidth": 480,
           "height": 230,
@@ -32,7 +32,7 @@ define([], function () {
           [
             {
               "key": "value",
-              "label": "Value for FY18 in Thousands of Dollars"
+              "label": "Value for FY20 in Thousands of Dollars"
             }
           ]
         ],
@@ -99,7 +99,7 @@ define([], function () {
           [
             {
               "key": "value",
-              "label": "Value for FY18 in Thousands of Dollars"
+              "label": "Value for FY20 in Thousands of Dollars"
             }
           ]
         ],
@@ -140,8 +140,8 @@ define([], function () {
         ]
       },
       {
-        "name": "Number of MBEs",
-        "element": "number-MBEs",
+        "name": "Total of Contracts with MBEs",
+        "element": "Total-of-contract-MBEs",
         "hot_parameters": {
           "rowHeaderWidth": 480,
           "height": 230,
@@ -151,22 +151,22 @@ define([], function () {
         "rows": [
           {
             "key": "local",
-            "label": "Number of Local MBEs With Whom You Have Done Business"
+            "label": "Total of Contracts with Local MBEs"
           },
           {
             "key": "state",
-            "label": "Number of State MBEs With Whom You Have Done Business"
+            "label": "Total of Contracts with State MBEs"
           },
           {
             "key": "national",
-            "label": "Number of National MBEs With Whom You Have Done Business"
+            "label": "Total of Contracts with National MBEs"
           }
         ],
         "cols": [
           [
             {
               "key": "value",
-              "label": "Value for FY18"
+              "label": "Value for FY20"
             }
           ]
         ],
@@ -176,18 +176,85 @@ define([], function () {
               "row": "*",
               "col": "*"
             },
-            "type": "int",
+            "type": "currency",
             "min": 0,
-            "max_warning": 10000,
+            "max_warning": 10000000,
             "empty": false
           }
         ],
         "excel": [
           {
-            "sheet": "3. Number of MBEs",
+            "sheet": "3. Total of Contracts",
             "start": "B2",
             "end": "B4",
-            "firstrow": "Number of Local MBEs With Whom You Have Done Business"
+            "firstrow": "Total of Contracts with Local MBEs"
+          }
+        ],
+        "tooltips": [
+          {
+            "range": {
+              "row": "*",
+              "col": "*"
+
+            },
+            "tooltip": {
+              "errorTitle": "Invalid Data Entry",
+              "error": "Please do not input any text or leave any cells blank. If the value is zero, please input zero.",
+              "warningTitle": "Warning: Data is too big",
+              "warning": "Are you sure this value is correct?"
+            }
+          }
+        ]
+      },
+      {
+        "name": "Value of Contracts",
+        "element": "value-of-contracts",
+        "hot_parameters": {
+          "rowHeaderWidth": 480,
+          "height": 230,
+          "colWidths": [190],
+          "stretchH": "last"
+        },
+        "rows": [
+          {
+            "key": "local",
+            "label": "Dollars Spent with Local MBEs"
+          },
+          {
+            "key": "state",
+            "label": "Dollars Spent with State MBEs"
+          },
+          {
+            "key": "national",
+            "label": "Dollars Spent with National MBEs"
+          }
+        ],
+        "cols": [
+          [
+            {
+              "key": "value",
+              "label": "Value for FY20"
+            }
+          ]
+        ],
+        "types": [
+          {
+            "range": {
+              "row": "*",
+              "col": "*"
+            },
+            "type": "currency",
+            "min": 0,
+            "max_warning": 10000000,
+            "empty": false
+          }
+        ],
+        "excel": [
+          {
+            "sheet": "4. Value of Contracts",
+            "start": "B2",
+            "end": "B4",
+            "firstrow": "Dollars Spent with Local MBEs"
           }
         ],
         "tooltips": [
@@ -208,23 +275,23 @@ define([], function () {
       }
     ],
     'usability': [
-      'data_prefilled', 
+      'data_prefilled',
       {'time_spent': ['page', 'session-area', 'tables-area', 'amount-spent', 'number-MBEs', 'addressable-spend', 'review-and-submit']},
       {'browser': ['chrome', 'edge', 'msie', 'firefox', 'opera', 'other', 'safari']},
       {'validation_errors': [
-        'SESSION_KEY_ERROR', 
+        'SESSION_KEY_ERROR',
         'SESSION_INFO_ERROR',
-        'PARTICIPATION_CODE_ERROR', 
-        'SESSION_PARTICIPATION_CODE_SERVER_ERROR', 
-        'UNCHECKED_ERR', 
-        'GENERIC_TABLE_ERR', 
-        'SERVER_ERR', 
+        'PARTICIPATION_CODE_ERROR',
+        'SESSION_PARTICIPATION_CODE_SERVER_ERROR',
+        'UNCHECKED_ERR',
+        'GENERIC_TABLE_ERR',
+        'SERVER_ERR',
         'GENERIC_SUBMISSION_ERR',
-        'NAN_EMPTY_CELLS', 
+        'NAN_EMPTY_CELLS',
         'SEMANTIC_CELLS',
         'CELL_ERROR'
         ]
       }
     ]
-  }
+  };
 });
