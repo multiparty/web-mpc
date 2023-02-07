21f987358bbd77b4131139b344245a62f072a712
Harunobu Ishii <harunobuishii547@gmail.com>
2023-02-07 16:16:02 -0500

    longer sleep before submission button

--- test/selenium/api/submission.js
+++ test/selenium/api/submission.js
@@ -80,7 +80,7 @@ module.exports = {
       }
     } while (! await verifyButton.isSelected());
     await driver.wait(until.elementIsEnabled(submitButton));
-    await driver.sleep(2500); // Sometimes submission doesn't trigger if button clicked too early 
+    await driver.sleep(5000); // Sometimes submission doesn't trigger if button clicked too early 
     await submitButton.click();
 
     // Verify success
